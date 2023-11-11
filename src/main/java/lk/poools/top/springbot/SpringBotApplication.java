03687ae318add658901758c5c4ee95f92bf0c786
Arukhan <Arukhan.kenes@gmail.com>
2023-11-11 19:26:39 +0600

    SpringBot init, tested

---
+++ src/main/java/lk/poools/top/springbot/SpringBotApplication.java
@@ -0,0 +1,14 @@
+package lk.poools.top.springbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBotApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
